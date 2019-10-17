2fc60d55eadba680a77f54e0d3e0b2267bc4c580
cosminandreicontu <cosminandrei.cnt@gmail.com>
2019-10-17 11:12:25 +0300

    Updates

---
+++ precache-manifest.15faec49f04aad09c53b01158870ab45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093be9dc81cb677b36f6e63eed7b7a9d",
+    "url": "/sampleReactApp/index.html"
+  },
+  {
+    "revision": "0b58edc59a829fd90c8c",
+    "url": "/sampleReactApp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f922d3c4bfbb141a859d",
+    "url": "/sampleReactApp/static/js/2.378e307a.chunk.js"
+  },
+  {
+    "revision": "0b58edc59a829fd90c8c",
+    "url": "/sampleReactApp/static/js/main.d7166bbd.chunk.js"
+  },
+  {
+    "revision": "77d5d5c3db7a20a151ff",
+    "url": "/sampleReactApp/static/js/runtime-main.d6fab2fb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sampleReactApp/static/media/logo.25bf045c.svg"
+  }
+]);
